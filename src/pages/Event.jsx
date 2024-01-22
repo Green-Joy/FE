25e0cff719f04024dc0ce95c32a4d0d0d3764c29
Osw0 <5196ksy@gmail.com>
2024-01-22 21:44:28 +0900

    design: feed,event ui fix

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,7 +1,191 @@
+import React, { useState } from "react";
+import {
+  Grid,
+  GridItem,
+  Card,
+  Image,
+  Stack,
+  CardBody,
+  CardFooter,
+  Heading,
+  Text,
+  Button,
+  Box,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+
 export default function Event() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <>
-      <p>축제 뉴스</p>
+      <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+        <Box>
+          <Heading size="lg" margin={10} textAlign="center">
+            Festival
+          </Heading>
+          <Card
+            direction={{ base: "column", sm: "row" }}
+            overflow="hidden"
+            variant="outline"
+            margin={6}
+            maxH="190px"
+            maxW="570px"
+          >
+            <Image
+              objectFit="cover"
+              maxW={{ base: "100%", sm: "200px" }}
+              src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+            />
+            <Stack
+              justifyContent={{ base: "flex-start", sm: "space-between" }}
+              flex="1"
+              ml={{ base: 0, sm: 4 }}
+            >
+              <CardBody maxH="130px" maxW="340px">
+                <Heading size="md" maxH="50px" overflow="hidden">
+                  축제 제목
+                </Heading>
+                <Text py="2" maxH="80px" overflow="hidden">
+                  미리보기 내용
+                </Text>
+              </CardBody>
+              <CardFooter justifyContent="flex-end">
+                <Button
+                  colorScheme="green"
+                  variant="link"
+                  onClick={handleOpenModal}
+                >
+                  See more ≫
+                </Button>
+              </CardFooter>
+            </Stack>
+          </Card>
+          <Modal isOpen={isModalOpen} onClose={handleCloseModal} size="xl">
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>축제 제목</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>내용</ModalBody>
+            </ModalContent>
+          </Modal>
+        </Box>
+
+        <Box>
+          <Heading size="lg" margin={10} textAlign="center">
+            News
+          </Heading>
+          <Card
+            direction={{ base: "column", sm: "row" }}
+            overflow="hidden"
+            variant="outline"
+            margin={6}
+            maxH="190px"
+            maxW="570px"
+          >
+            <Image
+              objectFit="cover"
+              maxW={{ base: "100%", sm: "200px" }}
+              src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+            />
+            <Stack
+              justifyContent={{ base: "flex-start", sm: "space-between" }}
+              flex="1"
+              ml={{ base: 0, sm: 4 }}
+            >
+              <CardBody maxH="130px" maxW="340px">
+                <Heading size="md" maxH="50px" overflow="hidden">
+                  뉴스 제목
+                </Heading>
+                <Text py="2" maxH="80px" overflow="hidden">
+                  미리보기 내용
+                </Text>
+              </CardBody>
+              <CardFooter justifyContent="flex-end">
+                <Button
+                  colorScheme="green"
+                  variant="link"
+                  onClick={handleOpenModal}
+                >
+                  See more ≫
+                </Button>
+              </CardFooter>
+            </Stack>
+          </Card>
+          <Modal isOpen={isModalOpen} onClose={handleCloseModal} size="xl">
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>뉴스 제목</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>내용</ModalBody>
+            </ModalContent>
+          </Modal>
+        </Box>
+
+        <Box>
+          <Heading size="lg" margin={10} textAlign="center">
+            Tips
+          </Heading>
+          <Card
+            direction={{ base: "column", sm: "row" }}
+            overflow="hidden"
+            variant="outline"
+            margin={6}
+            maxH="190px"
+            maxW="570px"
+          >
+            <Image
+              objectFit="cover"
+              maxW={{ base: "100%", sm: "200px" }}
+              src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+            />
+            <Stack
+              justifyContent={{ base: "flex-start", sm: "space-between" }}
+              flex="1"
+              ml={{ base: 0, sm: 4 }}
+            >
+              <CardBody maxH="130px" maxW="340px">
+                <Heading size="md" maxH="50px" overflow="hidden">
+                  팁 제목
+                </Heading>
+                <Text py="2" maxH="80px" overflow="hidden">
+                  미리보기 내용
+                </Text>
+              </CardBody>
+              <CardFooter justifyContent="flex-end">
+                <Button
+                  colorScheme="green"
+                  variant="link"
+                  onClick={handleOpenModal}
+                >
+                  See more ≫
+                </Button>
+              </CardFooter>
+            </Stack>
+          </Card>
+          <Modal isOpen={isModalOpen} onClose={handleCloseModal} size="xl">
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>팁 제목</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>내용</ModalBody>
+            </ModalContent>
+          </Modal>
+        </Box>
+      </Grid>
     </>
   );
 }
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import {
   Box,
@@ -14,11 +14,28 @@ import {
   SimpleGrid,
   CardFooter,
   Spacer,
+  Grid,
+  GridItem,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
 } from "@chakra-ui/react";
-import { FaRegHeart, FaHeart  } from "react-icons/fa";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
 
 export default function Feed() {
   const [isLoaded, setIsLoaded] = React.useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <>
       <Stack direction="row" spacing={4} align="center">
@@ -53,7 +70,20 @@ export default function Feed() {
             fadeDuration={1}
           >
             <CardBody>
-              <Heading size="md">환경축제!</Heading>
+              <Grid templateColumns="repeat(5, 1fr)" gap={4}>
+                <GridItem colSpan={2}>
+                  <Heading size="md">환경축제!</Heading>
+                </GridItem>
+                <GridItem colStart={4} colEnd={6}>
+                  <Button
+                    colorScheme="green"
+                    variant="link"
+                    onClick={handleOpenModal}
+                  >
+                    See more ≫
+                  </Button>
+                </GridItem>
+              </Grid>
             </CardBody>
             <CardFooter>
               <Flex spacing="1">
@@ -70,6 +100,14 @@ export default function Feed() {
             </CardFooter>
           </Skeleton>
         </Card>
+        <Modal isOpen={isModalOpen} onClose={handleCloseModal} size="xl">
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>축제 제목</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>내용</ModalBody>
+          </ModalContent>
+        </Modal>
         <Card>
           <Skeleton height="100%" isLoaded={isLoaded}>
             <CardBody>
@@ -86,7 +124,16 @@ export default function Feed() {
             fadeDuration={1}
           >
             <CardBody>
-              <Heading size="md">환경축제!</Heading>
+              <Grid templateColumns="repeat(5, 1fr)" gap={4}>
+                <GridItem colSpan={2}>
+                  <Heading size="md">환경축제!</Heading>
+                </GridItem>
+                <GridItem colStart={4} colEnd={6}>
+                  <Button colorScheme="green" variant="link">
+                    See more ≫
+                  </Button>
+                </GridItem>
+              </Grid>
             </CardBody>
             <CardFooter>
               <Flex spacing="1">
@@ -98,105 +145,6 @@ export default function Feed() {
                 </Flex>
               </Flex>
               <Button flex="1" variant="ghost" leftIcon={<FaRegHeart />}>
-                좋아요
-              </Button>
-            </CardFooter>
-          </Skeleton>
-        </Card>
-        <Card>
-          <Skeleton height="100%" isLoaded={isLoaded}>
-            <CardBody>
-              <Image
-                objectFit="cover"
-                src="https://file.newswire.co.kr/data/datafile2/thumb_640/2022/09/3420802290_20220923165542_7678832051.jpg"
-              />
-            </CardBody>
-          </Skeleton>
-          <Skeleton
-            height="100%"
-            width="100%"
-            isLoaded={isLoaded}
-            fadeDuration={1}
-          >
-            <CardBody>
-              <Heading size="md">환경축제!</Heading>
-            </CardBody>
-            <CardFooter>
-              <Flex spacing="1">
-                <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
-                  <Avatar src="https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_1280.jpg" />
-                  <Box>
-                    <Heading size="sm">닉네임</Heading>
-                  </Box>
-                </Flex>
-              </Flex>
-              <Button flex="1" variant="ghost" leftIcon={<FaRegHeart />}>
-                좋아요
-              </Button>
-            </CardFooter>
-          </Skeleton>
-        </Card>
-        <Card>
-          <Skeleton height="100%" isLoaded={isLoaded}>
-            <CardBody>
-              <Image
-                objectFit="cover"
-                src="https://file.newswire.co.kr/data/datafile2/thumb_640/2022/09/3420802290_20220923165542_7678832051.jpg"
-              />
-            </CardBody>
-          </Skeleton>
-          <Skeleton
-            height="100%"
-            width="100%"
-            isLoaded={isLoaded}
-            fadeDuration={1}
-          >
-            <CardBody>
-              <Heading size="md">환경축제!</Heading>
-            </CardBody>
-            <CardFooter>
-              <Flex spacing="1">
-                <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
-                  <Avatar src="https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_1280.jpg" />
-                  <Box>
-                    <Heading size="sm">닉네임</Heading>
-                  </Box>
-                </Flex>
-              </Flex>
-              <Button flex="1" variant="ghost" leftIcon={<FaRegHeart />}>
-                좋아요
-              </Button>
-            </CardFooter>
-          </Skeleton>
-        </Card>
-        <Card>
-          <Skeleton height="100%" isLoaded={isLoaded}>
-            <CardBody>
-              <Image
-                objectFit="cover"
-                src="https://file.newswire.co.kr/data/datafile2/thumb_640/2022/09/3420802290_20220923165542_7678832051.jpg"
-              />
-            </CardBody>
-          </Skeleton>
-          <Skeleton
-            height="100%"
-            width="100%"
-            isLoaded={isLoaded}
-            fadeDuration={1}
-          >
-            <CardBody>
-              <Heading size="md">환경축제!</Heading>
-            </CardBody>
-            <CardFooter>
-              <Flex spacing="1">
-                <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
-                  <Avatar src="https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_1280.jpg" />
-                  <Box>
-                    <Heading size="sm">닉네임</Heading>
-                  </Box>
-                </Flex>
-              </Flex>
-              <Button flex="1" variant="ghost" leftIcon={<FaHeart  />}>
                 좋아요
               </Button>
             </CardFooter>
