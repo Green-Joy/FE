86509639cc98a59b332519eb224253342e2e230c
SYEONIH <syeonih@naver.com>
2024-01-22 15:08:02 +0900

    로그인 ui

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,40 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-
-export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Username:', username);
-    console.log('Password:', password);
+import { Text,Divider,Box, Input, Button, FormControl, FormLabel, VStack } from '@chakra-ui/react';
+import { FaGoogle } from 'react-icons/fa';
+const LoginForm = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // 로그인 로직 
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={handleUsernameChange} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" value={password} onChange={handlePasswordChange} />
-      </label>
-      <br />
-      <button type="submit">Login</button>
-      <div>
-      Don't have an account? <Link to="/signup">Sign up</Link>
-      </div>
-    </form>
+    <VStack spacing={4} align="stretch" width="300px">
+      <Box p={4} borderWidth={1} borderRadius="md">
+        <form onSubmit={handleSubmit}>
+          <VStack spacing={4} align="stretch">
+            <FormControl>
+              <FormLabel>Email</FormLabel>
+              <Input type="email" placeholder="이메일을 입력하세요" />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Password</FormLabel>
+              <Input type="password" placeholder="비밀번호를 입력하세요" />
+            </FormControl>
+            <Button type="submit" colorScheme="teal">
+              로그인
+            </Button>
+            <Button leftIcon={<FaGoogle />}>
+    구글 로그인
+  </Button>
+  <Divider />
+  <Button  colorScheme='teal' variant='link'>
+  <Link to="/signup">Create account?</Link>
+  </Button>
+          </VStack>
+        </form>
+      </Box>
+    </VStack>
   );
-}
+};
+
+export default LoginForm;
