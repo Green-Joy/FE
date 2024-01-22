8caf2c9b90550004587294ef62b7ea00e373cd30
SYEONIH <syeonih@naver.com>
2024-01-22 14:53:14 +0900

    피드 올리기 ui

--- src/pages/EventUpload.jsx
+++ src/pages/EventUpload.jsx
@@ -1,7 +1,12 @@
+import { Image,Text,Box } from '@chakra-ui/react'
+
 export default function EventUpload() {
     return (
       <>
-        <p>꿀팁 올리기</p>
+        <Text></Text>
+        <Box boxSize='sm'>
+  <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+</Box>
       </>
     );
   }
--- src/pages/FeedUpload.jsx
+++ src/pages/FeedUpload.jsx
@@ -1,28 +1,38 @@
-import '../styles/FeedUpload.css'
+import { Image, Text, Box, Stack, Input, Button, Textarea } from '@chakra-ui/react';
+import '../styles/FeedUpload.css';
 
 export default function FeedUpload() {
-    return (
-      <>
-        <div id="feed-upload">
-            <div id="feed-image">
-                이미지 올리기
-            </div>
-            <div id="feed-info">
-                <div id="feed-title">
-                    제목
-                </div>
-                <div id="feed-detail">
-                    글쓰기
-                </div>
-                <div id="feed-loc">
-                    위치
-                </div>
-                <div id="feed-submit">
-                    <button> 올리기 </button>
-                </div>  
-            </div>
+  return (
+    <>
+      <Text>피드 작성</Text>
+      <div id="feed-upload">
+        <div id="feed-image">
+          <Box boxSize='sm'>
+            <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+            <Button colorScheme='teal' size='sm'>사진 업로드</Button>
+          </Box>
         </div>
-      </>
-    );
-  }
-  
+        <div id="feed-info">
+          <div id="feed-title">
+            <Stack spacing={3}>
+              <Input placeholder='medium size' size='md' placeholder='제목' width="500px" height="50px" />
+            </Stack>
+          </div>
+          <div id="feed-detail">
+            <Stack spacing={3}>
+            <Textarea placeholder='글쓰기' size='md' placeholder='글쓰기' width="500px" height="200px" />
+            </Stack>
+          </div>
+          <div id="feed-loc">
+            <Stack spacing={3}>
+              <Input placeholder='medium size' size='md' placeholder='위치정보' width="500px" height="50px" />
+            </Stack>
+          </div>
+          <div id="feed-submit">
+            <Button colorScheme='teal' size='sm'>올리기</Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
