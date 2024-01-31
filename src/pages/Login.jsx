d9815a837a8c7a46ec04c60bbcb59f1002f05edb
SYEONIH <syeonih@naver.com>
2024-01-31 16:42:30 +0900

    refactor : Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,31 +1,51 @@
 import React from 'react';
 import { Heading, Button } from '@chakra-ui/react';
 import { FaGoogle } from 'react-icons/fa';
-import axios from 'axios';
+import {GoogleLogin} from "@react-oauth/google";
+import {GoogleOAuthProvider} from "@react-oauth/google";
+import qs from 'qs';
+
 
 const LoginForm = () => {
-  const handleGoogleLogin = async () => {
-    try {
-      // Send a request to initiate Google OAuth 2.0 authentication
-      const response = await axios.get('https://greenjoy.dev/oauth2/authorization/google');
+  const AUTHORUIZE_URI = "https://greenjoy.dev/oauth2/authorization/google"
 
-      // Handle the response data
-      console.log(response.data); // This might be the Google login page URL or other necessary data
+  const queryStr = qs.stringify({
+    client_id: process.env.CLIENT_ID,
+    redirect_uri: window.location.href,
+    response_type: "token",
+    scope: "https://www.googleapis.com/auth/contacts.readonly",
+  });
 
-      // If you want to redirect based on the response, you can uncomment the following line
-      // window.location.href = response.data.redirectUrl;
-    } catch (error) {
-      // Handle errors that may occur during the Google login process
-      console.error('Google login error:', error);
-    }
+  const loginUri = AUTHORUIZE_URI + "?" + queryStr;
+
+  const [contactGroups, setContactGroups] = useState([]);
+
+  useEffect(() => {
+    fetch(PEOPLE_URI, {
+      headers: { Authorization: "Bearer " + access_token },
+    })
+      .then((response) => response.json())
+      .then((data) => setContactGroups(data.contactGroups));
+  }, [access_token]);
+
+
+  const responseGoogle = (response) => {
+    // Handle the response from Google authentication
+    console.log(response);
   };
 
   return (
     <>
-      <Heading as='h2' size='xl'  m={5}>Do you want to LogIn?</Heading>
-      <Button size='lg' leftIcon={<FaGoogle />} onClick={handleGoogleLogin}>
-        구글 로그인
-      </Button>
+      <Heading as='h2' size='xl' m={5}>로그인 하시겠습니까?</Heading>
+      <GoogleOAuthProvider clientId={clientId}>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="구글 로그인"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
+      </GoogleOAuthProvider>
     </>
   );
 };
