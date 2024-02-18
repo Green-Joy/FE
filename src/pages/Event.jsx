eea2a36cdb29b1a801fd793e8b3cff8b9c4af5d9
SYEONIH <syeonih@naver.com>
2024-02-18 16:53:28 +0900

    feat : event 연결 완료, feed 페이징(postId) 수정

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import {
   Grid,
@@ -19,10 +19,11 @@ import {
   ModalBody,
   ModalCloseButton,
   useDisclosure,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 
 export default function Event() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen: isOpen1, onOpen: onOpen1, onClose: onClose1 } = useDisclosure();
+  const { isOpen: isOpen2, onOpen: onOpen2, onClose: onClose2 } = useDisclosure();
   const [newsData, setNewsData] = useState([]);
   const [tipData, setTipData] = useState([]);
   const [selectedNews, setSelectedNews] = useState(null);
@@ -32,26 +33,20 @@ export default function Event() {
   useEffect(() => {
     const fetchNewsData = async () => {
       try {
-        const response = await axios.get("https://greenjoy.dev/api/articles");
+        const response = await axios.get('https://greenjoy.dev/api/articles');
         setNewsData(response.data);
       } catch (error) {
-        console.error("Error fetching news data:", error);
+        console.error('Error fetching news data:', error);
       }
     };
 
     const fetchTipData = async () => {
       try {
-        
-        const tipIds = [1, 2, 3]; // 수정필요
-        const tipPromises = tipIds.map(async (tipId) => {
-          const response = await axios.get(`https://greenjoy.dev/api/infos/${tipId}`);
-          return response.data;
-        });
-
-        const tipResults = await Promise.all(tipPromises);
-        setTipData(tipResults);
+        const tipResponse = await axios.get('https://greenjoy.dev/api/infos?size=5&page=0&sort=createdAt%2Cdesc');
+        const tipDataArray = tipResponse.data.content;
+        setTipData(tipDataArray);
       } catch (error) {
-        console.error("Error fetching tip data:", error);
+        console.error('Error fetching tip data:', error);
       } finally {
         setLoading(false);
       }
@@ -63,16 +58,16 @@ export default function Event() {
 
   const handleSeeMoreNews = (news) => {
     setSelectedNews(news);
-    onOpen();
+    onOpen1();
   };
 
   const handleSeeMoreTip = (tip) => {
     setSelectedTip(tip);
-    onOpen();
+    onOpen2();
   };
 
   return (
-    <Grid templateColumns={{ base: "1fr", md: "1fr 1fr" }} gap={6}>
+    <Grid templateColumns={{ base: '1fr', md: '1fr 1fr' }} gap={6}>
       {/* News Section */}
       <Box>
         <Heading size="lg" margin={10} textAlign="left">
@@ -82,22 +77,30 @@ export default function Event() {
           <p>Loading...</p>
         ) : (
           newsData.map((news, index) => (
-            <Card key={index} direction={{ base: "column", sm: "row" }} overflow="hidden" variant="outline" margin={6} maxH="190px" maxW="570px">
-              <Image objectFit="cover" maxW={{ base: "100%", sm: "200px" }} src={news.image} />
-              <Stack justifyContent={{ base: "flex-start", sm: "space-between" }} flex="1" ml={{ base: 0, sm: 4 }}>
+            <Card
+              key={index}
+              direction={{ base: 'column', sm: 'row' }}
+              overflow="hidden"
+              variant="outline"
+              margin={6}
+              maxH="190px"
+              maxW="570px"
+            >
+              <Image objectFit="cover" maxW={{ base: '100%', sm: '200px' }} src={news.image} />
+              <Stack justifyContent={{ base: 'flex-start', sm: 'space-between' }} flex="1" ml={{ base: 0, sm: 4 }}>
                 <CardBody maxH="130px" maxW="340px">
                   <Heading size="md" maxH="50px" overflow="hidden">
                     {news.title}
                   </Heading>
-                  <Text py="2" maxH="80px" overflow="hidden">
+                  <Text fontSize="md" py="2" maxH="80px" overflow="hidden">
                     {news.preview}
                   </Text>
                 </CardBody>
                 <CardFooter justifyContent="flex-end">
                   <Button colorScheme="green" variant="link" onClick={() => handleSeeMoreNews(news)}>
-                    See more ≫
+                    Preview
                   </Button>
-                  {/* Use Link component or a tag for navigation */}
+
                   <Link to={news.url} target="_blank" rel="noopener noreferrer">
                     <Button colorScheme="green" variant="link">
                       Read more
@@ -116,12 +119,7 @@ export default function Event() {
           <Heading size="lg" marginTop={10} marginLeft={10} marginBottom={4}>
             Tips
           </Heading>
-          <Button
-            colorScheme="green"
-            variant="solid"
-            marginTop={7}
-            marginLeft={20}
-          >
+          <Button colorScheme="green" variant="solid" marginTop={7} marginLeft={20}>
             <Link to="/eventupload">Create Tips</Link>
           </Button>
         </Stack>
@@ -129,15 +127,22 @@ export default function Event() {
           <p>Loading...</p>
         ) : (
           tipData.map((tip, index) => (
-            <Card key={index} direction={{ base: "column", sm: "row" }} overflow="hidden" variant="outline" margin={6} maxH="190px" maxW="570px">
-              <Image objectFit="cover" maxW={{ base: "100%", sm: "200px" }} src={tip.image1} />
-              <Stack justifyContent={{ base: "flex-start", sm: "space-between" }} flex="1" ml={{ base: 0, sm: 4 }}>
+            <Card
+              key={index}
+              direction={{ base: 'column', sm: 'row' }}
+              overflow="hidden"
+              variant="outline"
+              margin={6}
+              maxH="190px"
+              maxW="570px"
+            >
+              <Image objectFit="cover" maxW={{ base: '100%', sm: '200px' }} src={tip.thumbnail} />
+              <Stack justifyContent={{ base: 'flex-start', sm: 'space-between' }} flex="1" ml={{ base: 0, sm: 4 }}>
                 <CardBody maxH="130px" maxW="340px">
                   <Heading size="md" maxH="50px" overflow="hidden">
                     {tip.title}
                   </Heading>
-                  <Text py="2" maxH="80px" overflow="hidden">
-                    {/* You can use tip.content or another property for the preview */}
+                  <Text fontSize="md" py="2" maxH="80px" overflow="hidden">
                     {tip.content}
                   </Text>
                 </CardBody>
@@ -153,15 +158,29 @@ export default function Event() {
       </Box>
 
       {/* Modal for displaying detailed information */}
-      <Modal isOpen={isOpen} onClose={onClose} size="xl">
+      <Modal isOpen={isOpen1} onClose={onClose1} size="xl">
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>{selectedNews?.title || selectedTip?.title}</ModalHeader>
+          <ModalHeader>{selectedNews?.title}</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
             <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" textAlign="center">
-              <Image src={selectedNews?.image || selectedTip?.image1} />
-              <Text>{selectedNews?.preview || selectedTip?.content}</Text>
+              <Image src={selectedNews?.image} />
+              <Text>{selectedNews?.preview}</Text>
+            </Box>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+
+      <Modal isOpen={isOpen2} onClose={onClose2} size="xl">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{selectedTip?.title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" textAlign="center">
+              <Image src={selectedTip?.thumbnail} />
+              <Text>{selectedTip?.content}</Text>
             </Box>
           </ModalBody>
         </ModalContent>
